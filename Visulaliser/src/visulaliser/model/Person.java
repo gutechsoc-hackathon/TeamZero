649374162be28803cf4b714476c6c547d19dd4b7
Delagon <duncanadamson@mail.com>
2013-11-02 19:02:13 +0000

    changes X and Y of people

--- Visulaliser/src/visulaliser/model/Person.java
+++ Visulaliser/src/visulaliser/model/Person.java
@@ -1,12 +1,19 @@
 package visulaliser.model;
 import java.util.HashMap;
+import java.math.*;
+import java.util.Random;
 public class Person {
     private ID mID;
     private ID mNodeID;
     private HashMap<String, Message> mMessages;
     private int mX;
     private int mY;
+    private static Random randomGenerator = new Random();
 
+    public void changePosition(){
+        mX = randomGenerator.nextInt(200);
+        mY = randomGenerator.nextInt(200);                       
+    }
     
     public int getX(){
         return mX;
