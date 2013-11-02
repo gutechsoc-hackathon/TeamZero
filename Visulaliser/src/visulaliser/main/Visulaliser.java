f12bdcf16c688530b23fa1a9656e734a20645f9d
Delagon <duncanadamson@mail.com>
2013-11-02 22:30:29 +0000

    Updated Sim and Vis

--- Visulaliser/src/visulaliser/main/Visulaliser.java
+++ Visulaliser/src/visulaliser/main/Visulaliser.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 import visulaliser.model.*;
 
 public class Visulaliser {
-
+public static final int WIDTH = 600;
+public static final int HIEGHT = 400;
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -17,8 +18,11 @@ public class Visulaliser {
 	}
 	
 	public static void CreateAndShow() {
-		Simulator sim = new Simulator("glasgowhack.csv",10,10);
+		Simulator sim = new Simulator("glasgowhack.csv",10,10,10);
+                sim.setHieght(HIEGHT);
+                sim.setWidth(WIDTH);
 		JFrame frame = new JFrame("visulaliser");
+                frame.setSize(WIDTH, HIEGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(sim);
 		frame.pack();
