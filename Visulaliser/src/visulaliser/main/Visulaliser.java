216983ece6a1b9177fe217b96363f6dddda4747f
id3nt1ty <mr.nathan.collins@gmail.com>
2013-11-02 19:14:39 +0000

    Refactoring Visulaliser.java

--- Visulaliser/src/visulaliser/main/Visulaliser.java
+++ Visulaliser/src/visulaliser/main/Visulaliser.java
@@ -1,25 +1,29 @@
 package visulaliser.main;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import visulaliser.model.*;
+
 public class Visulaliser {
-    BufferedImage image = new BufferedImage(800,600,BufferedImage.TYPE_INT_ARGB);
-    public static void CreateAndShow(){
-        Node mNode = new Node(10,10,"a","a");
-        JFrame frame = new JFrame("visualiser");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(mNode);
-        frame.pack();
-        frame.setVisible(true);
-        }
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                // load data
-               
-                // run sim
-                CreateAndShow();
-    }
-        });
-}}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				// load data
+
+				// run sim
+				CreateAndShow();
+			}
+		});
+	}
+	
+	public static void CreateAndShow() {
+		Node mNode = new Node(10, 10, "a", "a");
+		JFrame frame = new JFrame("visualiser");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(mNode);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
