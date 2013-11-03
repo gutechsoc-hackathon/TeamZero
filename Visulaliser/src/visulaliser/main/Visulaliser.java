ca0e7e6f99d55d0c7496634a07aef32e7a019fa9
Delagon <duncanadamson@mail.com>
2013-11-03 15:19:31 +0000

    bugfix

--- Visulaliser/src/visulaliser/main/Visulaliser.java
+++ Visulaliser/src/visulaliser/main/Visulaliser.java
@@ -11,9 +11,9 @@ public class Visulaliser {
 	public static final int WIDTH = 600;
 	public static final int HIEGHT = 400;
 	public static final int NODES = MAX_NODES;
-	public static final int PEOPLE = 10;
-	public static final int ITERATIONS = 10;
-	public static final int MESSAGES = 10;
+	public static final int PEOPLE = 100;
+	public static final int ITERATIONS = 100;
+	public static final int MESSAGES = 100;
 	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
