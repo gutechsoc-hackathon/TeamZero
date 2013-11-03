eae8d9473487cd8b59b160eaf9e681c35fb19842
walnutmonster <filipbrakl@gmail.com>
2013-11-03 14:58:51 +0100

    Fixing test

--- Visulaliser/src/visulaliser/model/Test.java
+++ Visulaliser/src/visulaliser/model/Test.java
@@ -1,19 +1,19 @@
 package visulaliser.model;
+
 import java.util.ArrayList;
+
 public class Test {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Splitter testS=new Splitter();
-		ArrayList<Node> parsed=testS.networkParse("C:\\Users\\Walnutmonster\\Desktop\\hack (1)\\glasgowhack.csv");//random text parameter - not yet used
+		Splitter testS = new Splitter();
+		ArrayList<Node> parsed = testS
+				.networkParse("C:\\Users\\Walnutmonster\\Desktop\\hack (1)\\glasgowhack.csv");
 		int i;
-		for(i=0;i<6000;i++){
+		for (i = 0; i < 500; i++) {
 			System.out.println(parsed.get(i));
-			}
-	}Selector let
+		}
+	}
 }
-		
-
-
